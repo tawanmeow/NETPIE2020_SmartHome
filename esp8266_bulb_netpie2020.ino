72c700e43f7aa978d76b06680cca8c01187920c8
Tawan Hohum <57322704+tawanmeow@users.noreply.github.com>
2020-01-30 19:01:38 +0700

    Update esp8266_bulb_netpie2020.ino

--- esp8266_bulb_netpie2020.ino
+++ esp8266_bulb_netpie2020.ino
@@ -3,7 +3,6 @@
 
 #define PIR 15
 #define relay 4
-#define LED 13
 
 const char* ssid = "Your SSID";
 const char* password = "Your WiFi Password";
@@ -12,19 +11,20 @@ const int mqtt_port = 1883;
 const char* mqtt_Client = "Your Client ID";
 const char* mqtt_username = "Your Token";
 const char* mqtt_password = "Your Secret";
+
+char msg[50];
 int lighting = 0;
-int statePIR = 0;
-int stateLED = 0;
+int PIRstate = 0;
+int period = 1500;
+unsigned long countTime = 0;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-char msg[50];
-
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection…");
-    if (client.connect(mqtt_Client, mqtt_username, mqtt_password)) {
+    if (client.connect(mqtt_client, mqtt_username, mqtt_password)) {
       Serial.println("Connected");
       client.subscribe("@msg/LEDstatus");
     }
@@ -37,6 +37,19 @@ void reconnect() {
   }
 }
 
+void onoff(int lighting) {
+  if (lighting == 1){
+    Serial.println("Turn on the light!");
+    digitalWrite(relay,HIGH);
+    client.publish("@shadow/data/update", "{\"data\" : {\"LEDstatus\" : \"on\"}}");
+  } 
+  else if (lighting == 0) {
+    Serial.println("Turn off the light!");
+    digitalWrite(relay,LOW);
+    client.publish("@shadow/data/update", "{\"data\" : {\"LEDstatus\" : \"off\"}}");
+  }  
+}
+
 void callback(char* topic,byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
@@ -47,42 +60,22 @@ void callback(char* topic,byte* payload, unsigned int length) {
   }
    Serial.println(msg);
   if (String(topic) == "@msg/LEDstatus") {
-      convertState(msg); 
+    if (msg == "on"){
+      lighting = 1;
+      onoff(lighting); 
+    } 
+    else if (msg == "off") {
+      lighting = 0;
+      onoff(lighting);
+    } 
   }
-}
-
-void convertState(String msg) {
-  if (msg == "on"){
-    lighting = 1;
-  } 
-  else if (msg == "off") {
-    lighting = 0;
-  }
-  onoff(lighting); 
-}
-
-void onoff(int lighting) {
-  if (lighting == 1){
-    Serial.println("Turn on the light!");
-    digitalWrite(LED,HIGH);
-    digitalWrite(relay,HIGH);
-    client.publish("@shadow/data/update", "{\"data\" : {\"LEDstatus\" : \"on\"}}");
-  } 
-  else if (lighting == 0) {
-    Serial.println("Turn off the light!");
-    digitalWrite(LED,LOW);
-    digitalWrite(relay,LOW);
-    client.publish("@shadow/data/update", "{\"data\" : {\"LEDstatus\" : \"off\"}}");
-  }  
 }
 
 void setup() {
   Serial.begin(115200);
   Serial.println("HELLO :)");
   pinMode(relay,OUTPUT);
-  pinMode(LED,OUTPUT);
   pinMode(PIR,INPUT);
-  
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -90,7 +83,6 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
   Serial.println("WiFi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
@@ -99,26 +91,23 @@ void setup() {
 }
 
 void loop() {
-    if (!client.connected()) {
+  if (!client.connected()) {
     reconnect();
-    }
-    statePIR = digitalRead(PIR);
-    //int stateLED = digitalRead(LED);
-
-    if (statePIR == LOW) {
+  }
+  if(millis() >= countTime + period) {
+    PIRstate = digitalRead(PIR);
+    countTime += period;
+    if (PIRstate == LOW) {
       Serial.println("Motion absent");
-      delay(1200);
     }
-    else if (statePIR == HIGH) {
+    else if (PIRstate == HIGH) {
       Serial.println("Motion detected :)");
       lighting = lighting + 1;
       if (lighting == 2) {
         lighting = 0;
-      }  
-      onoff(lighting);
-      delay(6000);
+      }
+        onoff(lighting);  
+      } 
     }
-    client.loop();
+  client.loop();
 }
-
-//D8 D2 D7
